b0b179eca497717f829021b77b202df6e8821c8b
HP I5 <ronidasroni22@gmail.com>
2024-09-23 00:58:13 +0600

    First commit

---
+++ app/src/main/java/com/example/edumanage/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.edumanage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int display_msg_length = 2000; // Duration for the splash screen
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }, display_msg_length);
+    }
+}
