e44b459ecb5e8959b00da32f02894e1d6866d2bf
HP I5 <ronidasroni22@gmail.com>
2024-11-14 15:36:15 +0600

    Database Connection

--- app/src/main/java/com/example/edumanage/StudentProfileActivity.java
+++ app/src/main/java/com/example/edumanage/StudentProfileActivity.java
@@ -15,9 +15,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentProfileActivity extends AppCompatActivity {
-
+    DatabaseHelper databaseHelper;
     Button btnBack;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +26,62 @@ public class StudentProfileActivity extends AppCompatActivity {
         TextView tvInfo = findViewById(R.id.tv_student_info);
         btnBack = findViewById(R.id.btn_student_back);
 
+        databaseHelper = new DatabaseHelper(this);
+
         Intent getintent = getIntent();
         String studentUsername = getintent.getStringExtra("student_username");
+
+        showStudentInfo(studentUsername, ivImage, tvInfo);
 
         btnBack.setOnClickListener(v -> {
             Intent intent = new Intent(StudentProfileActivity.this, StudentActivity.class);
             startActivity(intent);
             finish();
         });
+    }
+
+    private void showStudentInfo(String studentUsername, ImageView ivImage, TextView tvInfo) {
+        byte[] imageBytes = databaseHelper.getStudentImage(studentUsername);
+        if (imageBytes != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            ivImage.setImageBitmap(bitmap);
+            ivImage.setVisibility(ImageView.VISIBLE);
+        } else {
+            Log.e("StudentProfile", "No image found for username: " + studentUsername);
+            ivImage.setVisibility(ImageView.GONE);
+        }
+
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DatabaseHelper.TABLE_REGISTERSTUDENT + " WHERE " + DatabaseHelper.COL_RSUSERNAME + "=?", new String[]{studentUsername});
+
+        if (cursor.moveToFirst()) {
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSNAME));
+            String f_name = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSFATHERNAME));
+            String email = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSEMAIL));
+            String address = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSADDRESS));
+            String dob = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSDOB));
+            String gender = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSGENDER));
+            String mobile = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RSMOBILE));
+
+            String userInfo = "Name:          " + name + "\n" +
+                    "Username:      " + studentUsername + "\n" +
+                    "Father Name:   " + f_name + "\n" +
+                    "Email:         " + email + "\n" +
+                    "Address:       " + address + "\n" +
+                    "Date of Birth: " + dob + "\n" +
+                    "Gender:        " + gender + "\n" +
+                    "Mobile:        " + mobile;
+
+            tvInfo.setText(userInfo);
+            tvInfo.setVisibility(TextView.VISIBLE);
+            btnBack.setVisibility(Button.VISIBLE);
+        } else {
+            Toast.makeText(this, "Error retrieving student information", Toast.LENGTH_SHORT).show();
+            tvInfo.setVisibility(TextView.GONE);
+            btnBack.setVisibility(Button.GONE);
+        }
+
+        cursor.close();
+        db.close();
     }
 }
--- app/src/main/java/com/example/edumanage/TeacherProfileActivity.java
+++ app/src/main/java/com/example/edumanage/TeacherProfileActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TeacherProfileActivity extends AppCompatActivity {
-
+    DatabaseHelper databaseHelper;
     Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +26,60 @@ public class TeacherProfileActivity extends AppCompatActivity {
         TextView tvInfo = findViewById(R.id.tv_teacher_info);
         btnBack = findViewById(R.id.btn_teacher_back);
 
+        databaseHelper = new DatabaseHelper(this);
+
         Intent getIntent = getIntent();
         String teacherUsername = getIntent.getStringExtra("teacher_username");
 
+        showTeacherInfo(teacherUsername, ivImage, tvInfo);
+        
         btnBack.setOnClickListener(v -> {
             Intent intent = new Intent(TeacherProfileActivity.this, TeacherActivity.class);
             startActivity(intent);
             finish();
         });
+    }
+
+    private void showTeacherInfo(String teacherUsername, ImageView ivImage, TextView tvInfo) {
+        byte[] imageBytes = databaseHelper.getTeacherImage(teacherUsername);
+        if (imageBytes != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            ivImage.setImageBitmap(bitmap);
+            ivImage.setVisibility(ImageView.VISIBLE);
+        } else {
+            Log.e("TeacherProfile", "No image found for username: " + teacherUsername);
+            ivImage.setVisibility(ImageView.GONE);
+        }
+
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DatabaseHelper.TABLE_REGISTERTEACHER + " WHERE " + DatabaseHelper.COL_RTUSERNAME + "=?", new String[]{teacherUsername});
+
+        if (cursor.moveToFirst()) {
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RTNAME));
+            String email = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RTEMAIL));
+            String address = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RTADDRESS));
+            String dob = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RTDOB));
+            String gender = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RTGENDER));
+            String mobile = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COL_RTMOBILE));
+
+            String teacherInfo = "Name:          " + name + "\n" +
+                    "Username:      " + teacherUsername + "\n" +
+                    "Email:         " + email + "\n" +
+                    "Address:       " + address + "\n" +
+                    "Date of Birth: " + dob + "\n" +
+                    "Gender:        " + gender + "\n" +
+                    "Mobile:        " + mobile + "\n" +
+                    "User:          Teacher";
+
+            tvInfo.setText(teacherInfo);
+            tvInfo.setVisibility(TextView.VISIBLE);
+            btnBack.setVisibility(Button.VISIBLE);
+        } else {
+            Toast.makeText(this, "Error retrieving teacher information", Toast.LENGTH_SHORT).show();
+            tvInfo.setVisibility(TextView.GONE);
+            btnBack.setVisibility(Button.GONE);
+        }
+        cursor.close();
+        db.close();
     }
 }
